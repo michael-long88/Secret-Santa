cca42953d1819469b35be6021a45ae50894fce8a
Michael <huskermdl@yahoo.com>
2019-07-14 13:44:06 -0400

    finished pairings method

---
+++ secret_santa.py
@@ -0,0 +1,85 @@
+import random
+import smtplib
+import ssl
+import yaml
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+class Person:
+    def __init__(self, name, email, invalid_match):
+        self.name = name
+        self.email = email
+        self.invalid_match = invalid_match
+
+    def __str__(self):
+        return f"{self.name}, {self.email}"
+
+
+class SecretSanta:
+    def __init__(self):
+        self.participants = SecretSanta.get_participants()
+        self.pairings = {}
+        self.recipient_check = 0
+
+    @staticmethod
+    def get_participants() -> list:
+        participants = []
+        with open('participants.yaml') as f:
+            participants_yaml = yaml.safe_load(f)
+            for participant in participants_yaml.values():
+                new_person = Person(participant['name'], participant['email'], participant['invalid_matches'])
+                participants.append(new_person)
+        return participants
+
+    def create_pairings(self):
+        self.pairings = {}
+        for participant in self.participants:
+            recipient = self.get_recipient(participant)
+            if participant.name == recipient.name:
+                return self.create_pairings()
+            self.pairings[participant.name] = recipient.name
+
+    def get_recipient(self, giver: Person) -> Person:
+        if self.recipient_check > 10:
+            return giver
+        random_number = random.randint(0, len(self.participants) - 1)
+        print(f"random number: {random_number}")
+        if self.participants[random_number].name in giver.invalid_match or \
+                self.participants[random_number].name in self.pairings.values():
+            self.recipient_check += 1
+            return self.get_recipient(giver)
+        self.recipient_check = 0
+        return self.participants[random_number]
+
+    # https://realpython.com/python-send-email/
+    def send_emails(self):
+        port = 465
+        password = input("Type your password and press enter: ")
+        sender_email = "holley.secret.santa@gmail.com"
+
+        # Create a secure SSL context
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+            server.login("holley.secret.santa", password)
+
+            for person in self.participants:
+                message = MIMEMultipart()
+                message['From'] = sender_email
+                message['To'] = sender_email
+                message['Subject'] = "Secret Santa drawing"
+                body = f"Hello, {person.name}. \n\nYou have drawn {self.pairings[person.name]} for this year's " \
+                    f"Secret Santa."
+                message.attach(MIMEText(body, 'plain'))
+                text = message.as_string()
+                server.sendmail(sender_email, sender_email, text)
+
+
+if __name__ == '__main__':
+    secret_santa = SecretSanta()
+    secret_santa.create_pairings()
+    print(secret_santa.pairings.values())
+    for gifter, giftee in secret_santa.pairings.items():
+        print(f"{gifter}, {giftee}")
+    secret_santa.send_emails()
